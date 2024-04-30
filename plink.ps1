147a48ae914520b5c4952d1fb74a256c5d09e153
tsaalbach95 <67101460+tsaalbach95@users.noreply.github.com>
2024-04-30 09:45:54 +0200

    Update plink.ps1

--- plink.ps1
+++ plink.ps1
@@ -1,3 +1,3 @@
 Using plink.exe on Windows to restart PoE on Port:
-plink -ssh "IP-Adresse" -l "username" -v -pw "password" -no-antispoof "swctrl poe set off id 44;sleep 10;swctrl poe set auto id 44"
+plink -ssh "IP-Adresse" -l "username" -pw "password" -no-antispoof "swctrl poe set off id 44;sleep 10;swctrl poe set auto id 44"
 
